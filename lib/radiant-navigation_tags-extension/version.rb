a279fd522d55c6e6bd71e4c0086d32d84b61737b
Tristan Koch <tristan@tknetwork.de>
2013-04-15 15:10:52 +0200

    Add gemspec

---
+++ lib/radiant-navigation_tags-extension/version.rb
@@ -0,0 +1,3 @@
+module RadiantNavigationTagsExtension
+  VERSION = "2.0"
+end
--- navigation_tags_extension.rb
+++ navigation_tags_extension.rb
@@ -1,21 +1,22 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
+require 'radiant-navigation_tags-extension/version'
 
 class NavigationTagsExtension < Radiant::Extension
-  version "2.0"
+  version RadiantNavigationTagsExtension::VERSION
   description "Makes building navigations much easier."
-  url "http://yourwebsite.com/navigation_tags"
-  
+  url  "https://github.com/gunn/navigation_tags"
+
   # define_routes do |map|
   #   map.connect 'admin/navigation_tags/:action', :controller => 'admin/navigation_tags'
   # end
-  
+
   def activate
     Page.send :include, NavigationTags
   end
-  
+
   def deactivate
     # admin.tabs.remove "Navigation Tags"
   end
-  
+
 end
